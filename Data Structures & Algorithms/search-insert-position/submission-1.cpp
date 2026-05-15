71cd93e44ae5c58376c7000d47a5d18ae34f086d
Franklin Kao <franklinkao813@gmail.com>
2026-05-14 23:12:35 -0500

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    // method: modified binary search
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0;
+        int end = n - 1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (target == nums[mid]) return mid;
+            else if (target < nums[mid]){
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start; // We didn't find anything
+    }
+};

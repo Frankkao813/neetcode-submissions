1fd19f48ad40ac0091c4efe6eabc5bdf2d6a9049
Franklin Kao <franklinkao813@gmail.com>
2026-07-27 00:00:25 -0500

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    bool dfs(int curr, int parent, vector<vector<int>>& graph, vector<bool>& visited){
+        visited[curr] = true;
+        
+        for (int node: graph[curr]){
+            if (node == parent) continue;
+            // Visited through another path: cycle detected.
+            if (visited[node]) return false;
+            // continue to recurse
+            if (!dfs(node, curr, graph, visited)) return false; 
+        }
+        // if we exit the loop without return false in the middle
+        return true;
+    }
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n-1) return false;
+        vector<vector<int>> graph(n);
+        for (const auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+
+        }
+        vector<bool> visited(n, false);
+        dfs(0, -1, graph, visited);
+
+        // A tree must contain every node in one connected component.
+        for (bool wasVisited : visited) {
+            if (!wasVisited) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
